9aa149be390df90c4c0c9469a4a35d77ab5f70af
joseph thepa <mmthejhb025@student.wethinkcode.co.za>
2026-05-22 02:50:48 +0200

    feature: updating app class

---
+++ src/main/java/MainApp/App.java
@@ -0,0 +1,10 @@
+package MainApp;
+
+public class App {
+    private String name;
+    private String email;
+
+    public  static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/test/java/AppTest/TestApp.java
@@ -0,0 +1,17 @@
+package AppTest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestApp {
+
+    @BeforeEach
+    public void runThisTestFirst(){
+        System.out.println("hello");
+    }
+    @Test
+    public void appFunction(){
+
+    }
+
+}
